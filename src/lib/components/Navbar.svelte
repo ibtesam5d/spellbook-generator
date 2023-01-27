811e5946d15063ec4570641658f7e27596338452
ezra <ezra.furry@gmail.com>
2023-01-27 15:33:52 -0500

    MAJOR navbar overhaul

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,6 +1,13 @@
-<nav class="w-16 flex flex-col items-center gap-4 m-4 px-6 py-10 bg-slate-500 rounded-full">
-	<a href="/">Home</a>
-	<ul>
-		<a href="/spells">Spells</a>
+<nav class="w-16 m-10 flex flex-col items-center gap-4">
+	<div class="w-16 h-16 flex items-center justify-center bg-slate-500 rounded-full">
+		<a href="/"><i class="mi mi-home" /></a>
+	</div>
+	<div class="w-16 h-16 flex items-center justify-center bg-slate-500 rounded-full">
+		<a href="/spellbooks"><i class="mi mi-book" /></a>
+	</div>
+	<ul class="w-16 mt-auto flex flex-col gap-4 bg-slate-500 rounded-full">
+		<a href="/" class="flex h-16 items-center justify-center"><i class="mi mi-settings" /></a>
+		<a href="/"><i class="mi mi-user" /></a>
+		<a href="/" class="flex h-16 items-center justify-center"><i class="mi mi-log-in" /></a>
 	</ul>
 </nav>
