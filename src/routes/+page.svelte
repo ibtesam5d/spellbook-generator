2c39fa716fcd38df04bab812210340f481453f65
ezra <ezra.furry@gmail.com>
2023-01-28 21:09:31 -0500

    homepage kinda beat

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <section class="flex flex-col flex-auto">
 	<h1 class="text-5xl py-8">Spellbook Generator</h1>
-	<div class="flex flex-auto justify-around items-center">
-		<div class="flex flex-col">
+	<div class="mt-8 gap-16 flex flex-col flex-auto items-center">
+		<div class="p-8 rounded-full bg-slate-600/75">
 			<a href="/spellbooks/create" class="text-2xl">Create a Spellbook</a>
 		</div>
-		<div class="flex flex-col">
+		<div class="p-8 rounded-full bg-slate-600/75">
 			<a href="/spellbooks" class="text-2xl">Your Spellbooks</a>
 		</div>
 	</div>
