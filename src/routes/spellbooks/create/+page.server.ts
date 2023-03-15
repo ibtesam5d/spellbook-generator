f407e4fd6e7b97fc6b8e4e37732f6f096117dd24
jordan <jordanrwm@gmail.com>
2023-03-15 19:02:34 -0400

    I think we're back!

--- src/routes/spellbooks/create/+page.server.ts
+++ src/routes/spellbooks/create/+page.server.ts
@@ -1,6 +1,6 @@
 import { fail, redirect } from '@sveltejs/kit';
 import type { Actions } from './$types';
-import prisma from '$lib/prisma';
+import prisma from '$lib/server/prisma';
 
 export const actions = {
 	create: async ({ request }) => {
