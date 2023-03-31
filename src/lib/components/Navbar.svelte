fe1682aabc508f74de0cfc9c11651bb50c67762c
jordan <jordanrwm@gmail.com>
2023-03-31 15:24:39 -0400

    new icons, navbar refactor, svelte upgrade

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,43 +1,38 @@
-<nav class="w-16 h-screen m-8 flex flex-col items-center gap-4">
-	<ul class="nav-bubble">
-		<a href="/"><i class="mi-lg mi-home" /></a>
+<script lang="ts">
+	import {
+		Icon,
+		Home,
+		BookOpen,
+		Sparkles,
+		Cog8Tooth,
+		UserCircle,
+		ArrowRightOnRectangle
+	} from 'svelte-hero-icons';
+</script>
+
+<nav class="w-16 h-[calc(100vh-4rem)] m-8 flex flex-col items-center gap-4">
+	<ul class="rounded-full bg-yellow-600">
+		<a href="/" class="w-16 h-16 flex flex-col items-center justify-center"
+			><Icon src={Home} mini size="40" /></a
+		>
 	</ul>
-	<ul class=" nav-bubble">
-		<a href="/spellbooks"><i class="mi-lg mi-book" /></a>
-		<a href="/spells"><i class="mi-lg mi-wind" /></a>
+	<ul class="rounded-full bg-yellow-600">
+		<a href="/spellbooks" class="w-16 h-16 flex flex-col items-center justify-center"
+			><Icon src={BookOpen} mini size="40" /></a
+		>
+		<a href="/spells" class="w-16 h-16 flex flex-col items-center justify-center"
+			><Icon src={Sparkles} mini size="40" /></a
+		>
 	</ul>
-	<ul class="nav-bubble">
-		<a href="/"><i class="mi-lg mi-settings" /></a>
-		<a href="/"><i class="mi-lg mi-user" /></a>
-		<a href="/"><i class="mi-lg mi-log-in" /></a>
+	<ul class="mt-auto bg-yellow-600 rounded-full">
+		<a href="/" class="w-16 h-16 flex flex-col items-center justify-center"
+			><Icon src={Cog8Tooth} mini size="40" /></a
+		>
+		<a href="/" class="w-16 h-16 flex flex-col items-center justify-center"
+			><Icon src={UserCircle} mini size="40" /></a
+		>
+		<a href="/" class="w-16 h-16 flex flex-col items-center justify-center"
+			><Icon src={ArrowRightOnRectangle} mini size="40" /></a
+		>
 	</ul>
 </nav>
-
-<style lang="scss">
-	.h-screen {
-		height: calc(100vh - 4rem);
-	}
-
-	.nav-bubble {
-		width: 4rem;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		border-radius: 9999px;
-		@apply bg-yellow-600;
-
-		&:last-of-type {
-			margin-top: auto;
-		}
-	}
-
-	.nav-bubble a {
-		height: 4rem;
-		width: 4rem;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		border-radius: 9999px;
-	}
-</style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,5 +14,6 @@ module.exports = {
 		},
 		extend: {}
 	},
-	plugins: [require('tailwind-scrollbar')]
+	plugins: [require('tailwind-scrollbar')],
+	darkMode: 'class'
 };
